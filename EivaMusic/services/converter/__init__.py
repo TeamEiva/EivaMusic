619e51a84ced3636810e093e3a75006d108f6070
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-07-12 12:31:00 +0530

    Create __init__.py

---
+++ EivaMusic/services/converter/__init__.py
@@ -0,0 +1,11 @@
+from os import listdir
+
+from os import mkdir
+
+if 'raw_files' not in listdir():
+
+    mkdir('raw_files')
+
+from EivaMusic.services.converter.converter import convert
+
+__all__ = ["convert"]
