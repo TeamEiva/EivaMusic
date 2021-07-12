31ddacf023b670376ce8683e8d1680dad54782ef
SHIVANSH <75574324+errorshivansh@users.noreply.github.com>
2021-07-12 12:07:09 +0530

    Create __init__.py

---
+++ EivaMusic/services/queues/__init__.py
@@ -0,0 +1,11 @@
+from EivaMusic.services.queues.queues import clear 
+
+from EivaMusic.services.queues.queues import get
+
+from EivaMusic.services.queues.queues import is_empty
+
+from EivaMusic.services.queues.queues import put
+
+from EivaMusic.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
